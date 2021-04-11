8788f8060964f3a190ead88bf48de467acf30cf0
Besian I. Sejdiu <besiansejdiu@gmail.com>
2021-04-11 03:27:07 -0500

    add long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='prolintpy',
     version='0.9.0',
     description='Automated analyis and visualization of lipid-protein interactions.',
+    url="https://github.com/ProLint/prolintpy",
     author='Besian I. Sejdiu',
+    author_email="besian.sejdiu@stjude.org",
     license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires='>=3.6',
     packages=find_packages(),
     install_requires=[
